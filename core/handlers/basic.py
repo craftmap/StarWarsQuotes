d2a8e97ca529177347da8ba9ad9b7d1a914979c6
max_bestplace <mmukhametshin@bestplace.pro>
2024-01-04 19:52:39 +0300

    fix: typo

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -18,7 +18,7 @@ async def command_random_quote(message: Message) -> None:
     cursor.execute(f'SELECT * FROM {table_name} ORDER BY RAND() LIMIT 1;')
     doc = cursor.fetchone()
 
-    await message.answer(f'<b>Quote</b>:\n{doc["quote"]}\n©{doc["author_en"]}\n\n<b>Цитата</b>:\n<tg-spoiler>{doc["quote_translation"]}\n{doc["author_ru"]}</tg-spoiler>')
+    await message.answer(f'<b>Quote</b>:\n{doc["quote"]}\n©<i>{doc["author_en"]}</i>\n\n<b>Цитата</b>:\n<tg-spoiler>{doc["quote_translation"]}\n©<i>{doc["author_ru"]}</i></tg-spoiler>')
     await message.bot.send_message(getenv('ADMIN_ID'), f'{message.from_user.full_name} command rand\nGot: {doc["quote"]}')
     connection.close()
     cursor.close()
