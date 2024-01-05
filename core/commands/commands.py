623209d1b98ae883b85b57d5bf8927ad35ab8229
max_bestplace <mmukhametshin@bestplace.pro>
2024-01-05 21:10:05 +0300

    fix: add notify_the_creator

--- core/commands/commands.py
+++ core/commands/commands.py
@@ -5,24 +5,12 @@ from aiogram.types import BotCommand, BotCommandScopeDefault, InlineKeyboardButt
 async def set_commands(bot: Bot):
     commands = [
         BotCommand(
-            command='start',
-            description='Начало работы',
-        ),
-        BotCommand(
-            command='dice',
-            description='Бросить кубик',
-        ),
-        BotCommand(
             command='rand',
             description='Выдать случайную цитату',
         ),
         BotCommand(
             command='help',
             description='Помощь',
-        ),
-        BotCommand(
-            command='add',
-            description=f'/add <автор>: <цитата> — добавить цитату',
         ),
         BotCommand(
             command='rand_from_chat',
